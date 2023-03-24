5ad2b59dcb884a60e512decba3ecf25e5060cc70
UmidjanUbaydullayev@gmail.com <UmidjanUbaydullayev@gmail.com>
2023-03-24 23:19:51 +0100

    new change

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
-
+System.Console.WriteLine("Type 1st number: ");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
-firstNumber %= 10;
-System.Console.WriteLine(firstNumber);
+System.Console.WriteLine("Type 2nd number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"1st Number is grater than 2nd number: {firstNumber > secondNumber}");
+System.Console.WriteLine($"1st Num equal to 2nd Num: {firstNumber == secondNumber}");
+System.Console.WriteLine($"1st Num not equal to 2nd Num: {firstNumber != secondNumber}");
+System.Console.WriteLine($"1st Num less than or equal to 2nd Num: {firstNumber <= secondNumber}");
+System.Console.WriteLine($"1st Num greater than or equal to 2nd Num: {firstNumber >= secondNumber}");
