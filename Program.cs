0a71f8f44b5da71ee1a045706490147883974a21
UmidjanUbaydullayev@gmail.com <UmidjanUbaydullayev@gmail.com>
2023-03-29 19:51:27 +0100

    if else statement

--- Program.cs
+++ Program.cs
@@ -3,8 +3,21 @@ System.Console.WriteLine("Type 1st number: ");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Type 2nd number: ");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine($"1st Number is grater than 2nd number: {firstNumber > secondNumber}");
-System.Console.WriteLine($"1st Num equal to 2nd Num: {firstNumber == secondNumber}");
-System.Console.WriteLine($"1st Num not equal to 2nd Num: {firstNumber != secondNumber}");
-System.Console.WriteLine($"1st Num less than or equal to 2nd Num: {firstNumber <= secondNumber}");
-System.Console.WriteLine($"1st Num greater than or equal to 2nd Num: {firstNumber >= secondNumber}");
+if (firstNumber > secondNumber)
+{
+    System.Console.WriteLine("1st number is greater than 2nd number");
+}
+else if (firstNumber == secondNumber)
+{
+    System.Console.WriteLine("1st number equal to 2nd number");
+}
+else
+{
+    System.Console.WriteLine("1st num less than or equal to 2nd num");
+}
+
+// System.Console.WriteLine($"1st Number is grater than 2nd number: {firstNumber > secondNumber}");
+// System.Console.WriteLine($"1st Num equal to 2nd Num: {firstNumber == secondNumber}");
+// System.Console.WriteLine($"1st Num not equal to 2nd Num: {firstNumber != secondNumber}");
+// System.Console.WriteLine($"1st Num less than or equal to 2nd Num: {firstNumber <= secondNumber}");
+// System.Console.WriteLine($"1st Num greater than or equal to 2nd Num: {firstNumber >= secondNumber}");
