2751e5844b2fefcc9ddeb589105ba8b75c4cc398
UmidjanUbaydullayev@gmail.com <UmidjanUbaydullayev@gmail.com>
2023-04-02 20:35:41 +0200

    new

--- Program.cs
+++ Program.cs
@@ -1,8 +1,17 @@
 ﻿using System;
 System.Console.WriteLine("Type 1st number: ");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
+//Switch operator
+System.Console.WriteLine("Enter any operation that you want to proccess with (-,+,*,/,%):   ");
+string operation = Console.ReadLine();
 System.Console.WriteLine("Type 2nd number: ");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
+//Ternary Operator
+string message = (firstNumber >= 0)
+        ? "First number is positive"
+        : "First number is negative";
+System.Console.WriteLine(message);
+
 if (firstNumber > secondNumber)
 {
     System.Console.WriteLine("1st number is greater than 2nd number");
@@ -13,11 +22,26 @@ else if (firstNumber == secondNumber)
 }
 else
 {
-    System.Console.WriteLine("1st num less than or equal to 2nd num");
+    System.Console.WriteLine("1st number less than or equal to 2nd number");
 }
-
-// System.Console.WriteLine($"1st Number is grater than 2nd number: {firstNumber > secondNumber}");
-// System.Console.WriteLine($"1st Num equal to 2nd Num: {firstNumber == secondNumber}");
-// System.Console.WriteLine($"1st Num not equal to 2nd Num: {firstNumber != secondNumber}");
-// System.Console.WriteLine($"1st Num less than or equal to 2nd Num: {firstNumber <= secondNumber}");
-// System.Console.WriteLine($"1st Num greater than or equal to 2nd Num: {firstNumber >= secondNumber}");
+// switch operations
+string result = operation switch
+{
+        "+" => $"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}",
+        "-" => $"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}",
+        "/" => $"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}",
+        "*" => $"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}",
+        "%" => $"{firstNumber} % {secondNumber} = {firstNumber % secondNumber}",
+        _ => "Operation not found!"
+}; 
+ System.Console.WriteLine(result); 
+ // while loop
+ int counter = 1;
+while(counter < firstNumber)
+{
+    if(counter % 2 == 0)
+    {
+        System.Console.WriteLine(counter);
+    }
+    counter++;
+}
